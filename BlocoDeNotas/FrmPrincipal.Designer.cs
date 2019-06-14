cd9a317141f3339cf6eabb22832ee997caf985ab
Abner Castro <abnerfcastro@gmail.com>
2019-06-14 12:42:36 -0300

    Initial commit

---
+++ BlocoDeNotas/FrmPrincipal.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace BlocoDeNotas
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtConteudo = new System.Windows.Forms.TextBox();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnSalvarComo = new System.Windows.Forms.Button();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtConteudo
+            // 
+            this.txtConteudo.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConteudo.Location = new System.Drawing.Point(12, 41);
+            this.txtConteudo.Multiline = true;
+            this.txtConteudo.Name = "txtConteudo";
+            this.txtConteudo.Size = new System.Drawing.Size(776, 397);
+            this.txtConteudo.TabIndex = 0;
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(12, 11);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrir.TabIndex = 1;
+            this.btnAbrir.Text = "Abrir...";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnSalvarComo
+            // 
+            this.btnSalvarComo.Location = new System.Drawing.Point(93, 11);
+            this.btnSalvarComo.Name = "btnSalvarComo";
+            this.btnSalvarComo.Size = new System.Drawing.Size(109, 23);
+            this.btnSalvarComo.TabIndex = 2;
+            this.btnSalvarComo.Text = "Salvar como...";
+            this.btnSalvarComo.UseVisualStyleBackColor = true;
+            this.btnSalvarComo.Click += new System.EventHandler(this.btnSalvarComo_Click);
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "ofd";
+            this.ofd.Filter = "Arquivos de Texto (*.txt) | *.txt";
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Enabled = false;
+            this.btnSalvar.Location = new System.Drawing.Point(208, 11);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 3;
+            this.btnSalvar.Text = "Salvar...";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnSalvarComo);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.txtConteudo);
+            this.MaximizeBox = false;
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bloco de Notas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtConteudo;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnSalvarComo;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.Button btnSalvar;
+    }
+}
+
---
+++ BlocoDeNotas/FrmPrincipal.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BlocoDeNotas
+{
+    public partial class FrmPrincipal : Form
+    {
+        private string nomeArquivoAberto = null;
+
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+            this.Text = "Bloco de Notas - Sem título";
+        }
+
+        /// <summary>
+        /// Click Handler do botão 'Salvar Como'
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSalvarComo_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.Filter = "Arquivos de Texto (*.txt) | *.txt";
+
+            sfd.InitialDirectory = Environment.GetFolderPath(
+                Environment.SpecialFolder.MyDocuments);
+
+            sfd.RestoreDirectory = true;
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (var arquivo = sfd.OpenFile())
+                    {
+                        nomeArquivoAberto = sfd.FileName;
+                        btnSalvar.Enabled = true;
+                        this.Text = $"Bloco de Notas - {nomeArquivoAberto}";
+
+                        using (StreamWriter sw = new StreamWriter(arquivo))
+                        {
+                            sw.Write(txtConteudo.Text);
+                        }
+                    }
+
+                    MessageBox.Show(
+                        "O arquivo foi salvo com sucesso!",
+                        "Arquivo Salvo",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Click Handler do botão Abrir
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            ofd.InitialDirectory = Environment.GetFolderPath(
+                Environment.SpecialFolder.MyDocuments);
+
+            ofd.FileName = "";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (var arquivo = ofd.OpenFile())
+                    {
+                        nomeArquivoAberto = ofd.FileName;
+                        btnSalvar.Enabled = true;
+                        this.Text = $"Bloco de Notas - {nomeArquivoAberto}";
+
+                        using (StreamReader sr = new StreamReader(arquivo))
+                        {
+                            txtConteudo.Text = sr.ReadToEnd();
+                        }
+                    }
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(
+                        exception.Message,
+                        "Erro ao abrir o arquivo",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Click Handler para o botão Salvar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (nomeArquivoAberto == null)
+                return;
+
+            try
+            {
+                using (var arquivo = File.Open(nomeArquivoAberto, FileMode.Truncate))
+                {
+                    using (StreamWriter sw = new StreamWriter(arquivo))
+                    {
+                        sw.Write(txtConteudo.Text);
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(
+                    exception.Message,
+                    "Erro ao salvar o arquivo",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+    }
+}
